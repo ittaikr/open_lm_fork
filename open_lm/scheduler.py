1e3d462ff766a777afa67c33e51235b698820c67
tomerporian <tomerpor@gmail.com>
2024-04-04 23:14:24 +0200

    sidekick runs

--- open_lm/scheduler.py
+++ open_lm/scheduler.py
@@ -52,3 +52,63 @@ def cosine_lr(optimizer, base_lr, warmup_length, steps, min_lr, force_min_lr):
         assign_learning_rate(optimizer, lr)
         return lr
     return _lr_adjuster
+
+def cosine_lr_cycled(optimizer, base_lr, warmup_length, steps, min_lr, force_min_lr):
+    def _lr_adjuster(step):
+        if step < warmup_length:
+            lr = _warmup_lr(base_lr, warmup_length, step)
+        else:
+            e = step - warmup_length
+            es = steps - warmup_length
+            lr = min_lr + 0.5 * (1 + np.cos(np.pi * e / es)) * (base_lr - min_lr)
+            lr = max(lr, force_min_lr)
+        assign_learning_rate(optimizer, lr)
+        return lr
+    return _lr_adjuster
+
+def hybrid_cosine_rsqrt(optimizer, base_lr, warmup_length, steps, min_lr, d = 2.0):
+    def _lr_adjuster(step):
+        es = steps - warmup_length
+        C = (1 + np.cos(np.pi / d)) / 2
+        beta = (min_lr / (base_lr - min_lr) + C) * es / (np.sin(np.pi / d) * np.pi) - (es / d)
+        alpha = ((base_lr - min_lr) * C - min_lr) * np.sqrt(beta + es / d)
+        if step < warmup_length:
+            lr = _warmup_lr(base_lr, warmup_length, step)
+        elif step <= steps / d:
+            e = step - warmup_length
+            # es = steps - warmup_length
+            lr = min_lr + 0.5 * (1 + np.cos(np.pi * e / es)) * (base_lr - min_lr)
+        else:
+            e = step - warmup_length
+            lr = alpha / np.sqrt(e + beta)
+        lr_ret = max(lr, min_lr)
+        assign_learning_rate(optimizer, lr_ret)
+        return lr_ret
+    return _lr_adjuster
+
+def hybrid_cosine_rsqrt_cooldown(optimizer, base_lr, warmup_length, steps, min_lr, cooldown_steps, d = 2.0, cooldown_power=1.0, cooldown_end_lr=0.):
+    def _lr_adjuster(step):
+        es = steps - warmup_length
+        C = (1 + np.cos(np.pi / d)) / 2
+        beta = (min_lr / (base_lr - min_lr) + C) * es / (np.sin(np.pi / d) * np.pi) - (es / d)
+        alpha = ((base_lr - min_lr) * C - min_lr) * np.sqrt(beta + es / d)
+        start_cooldown_step = steps - cooldown_steps
+        if step < warmup_length:
+            lr = _warmup_lr(base_lr, warmup_length, step)
+        elif step <= steps / d:
+            e = step - warmup_length
+            # es = steps - warmup_length
+            lr = min_lr + 0.5 * (1 + np.cos(np.pi * e / es)) * (base_lr - min_lr)
+        elif step < start_cooldown_step:
+            e = step - warmup_length
+            lr = alpha / np.sqrt(e + beta)
+        else:
+            e = step - start_cooldown_step
+            es = steps - start_cooldown_step
+            # linear decay if power == 1; polynomial decay otherwise;
+            decay = (1 - (e/es)) ** cooldown_power
+            lr = decay * (alpha / np.sqrt(start_cooldown_step + beta)) + cooldown_end_lr
+        lr_ret = max(lr, min_lr)
+        assign_learning_rate(optimizer, lr_ret)
+        return lr_ret
+    return _lr_adjuster
