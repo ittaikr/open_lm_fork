86744fd2c0f399d8a553d861806bcaa2f5c8c326
tomerporian <tomerpor@gmail.com>
2024-04-13 13:17:10 +0200

    log every 100 steps of schedulefree model

--- scripts/jsc_script_2_nodes_laionize_eval.sh
+++ scripts/jsc_script_2_nodes_laionize_eval.sh
@@ -52,4 +52,4 @@ cd ${OPEN_CLIP_HOME}
 LOGS="/p/scratch/ccstdl/porian1/$3"
 
 WANDB_MODE=offline
-srun --cpu_bind=v --accel-bind=gn --threads-per-core=1 python -u -m open_lm.main --name "$2" --logs $LOGS --val-data "./random_val_shards/*"  --config $1 
+srun --cpu_bind=v --accel-bind=gn --threads-per-core=1 python -u -m open_lm.main --name "$2" --logs $LOGS --val-data "/p/fastdata/mmlaion/lmdata/rpj/shard_{00040319..00040338}.tar"  --config $1 
