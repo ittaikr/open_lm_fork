44807ff88a976f4506332c281db711497466348a
tomerporian <tomerpor@gmail.com>
2024-04-25 11:19:55 +0200

    saving ckpts by flops - to check on tau

--- open_lm/saving_utils.py
+++ open_lm/saving_utils.py
@@ -2,41 +2,46 @@ import logging
 import os
 import torch
 
+def unwrap_model(model):
+    return model.module if hasattr(model, 'module') else model
+
 def save_checkpoint_step(args, model, completed_flop, epoch, averagers, current_step):
-
-    checkpoint_dict_model = {
-        "epoch": epoch,
-        "flop": completed_flop,
-        "name": args.name,
-        "state_dict": model.state_dict(),
-        "step": current_step,
-    }
-
-    # if there will be more than args.max_checkpoints_flops checkpoints, remove the oldest one
-    # first, count the number of files that have "flop_" in their name
+    if os.path.exists(os.path.join(args.checkpoint_path, f"flop_{completed_flop:.2e}.pt")):
+        return # in case of resuming, we don't want to save the same checkpoint twice
     flop_file_counter = 0
     for file in os.listdir(args.checkpoint_path):
         if "flop_" in file:
             flop_file_counter += 1
-    
-    if os.path.exists(os.path.join(args.checkpoint_path, f"flop_{completed_flop:.2e}.pt")):
-        return # in case of resuming, we don't want to save the same checkpoint twice
-    if flop_file_counter >= args.max_checkpoints_flops:
+    num_files_to_save = 1
+    if averagers is not None:
+        num_files_to_save += len(averagers.avgs_dict.keys())
+    if flop_file_counter >= args.max_checkpoints_flops * num_files_to_save:
         oldest_step = min([int(file.split("_")[2].split(".")[0]) for file in os.listdir(args.checkpoint_path) if "flop_" in file])
-        # oldest_flop = min([float(file.split("_")[1].split(".")[0]) for file in os.listdir(args.checkpoint_path) if "flop_" in file])
-        # remove all files that have the oldest flop in their name, including averagers
         for file in os.listdir(args.checkpoint_path):
             if f"_{oldest_step}" in file:
                 os.remove(os.path.join(args.checkpoint_path, file))
-
-
+    
+    checkpoint_dict_model = {
+        "epoch": epoch,
+        "flop": completed_flop,
+        "name": args.name,
+        "state_dict": unwrap_model(model).state_dict(),
+        "step": current_step,
+    }
     torch.save(
             checkpoint_dict_model,
-            os.path.join(args.checkpoint_path, f"flop_{completed_flop:.2e}_{current_step}.pt"),
+            os.path.join(args.checkpoint_path, f"flop_{completed_flop:.2e}_step_{current_step}.pt"),
         )
     if averagers is not None:
         for k in averagers.avgs_dict:
+            checkpoint_dict_model = {
+                "epoch": epoch,
+                "flop": completed_flop,
+                "name": args.name,
+                "state_dict": averagers.avgs_dict[k].state_dict(),
+                "step": current_step,
+            }
             torch.save(
                 averagers.avgs_dict[k].get_state_dict_avg(),
-                os.path.join(args.checkpoint_path, f"{k}_{completed_flop:.2e}_{current_step}.pt"),
+                os.path.join(args.checkpoint_path, f"flop_{completed_flop:.2e}_step_{current_step}_{k}.pt"),
             )
